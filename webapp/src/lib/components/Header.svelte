55ee2901dd63b40c4b526d2abc80e530f64f2e65
Jacobtread <jacobtread@gmail.com>
2024-07-07 17:32:03 +1200

    feat: header navigation and new pages

--- webapp/src/lib/components/Header.svelte
+++ webapp/src/lib/components/Header.svelte
@@ -1,14 +1,65 @@
-<div class="sidebar">
+<script lang="ts">
+	import { base } from '$app/paths';
+
+	import EventsIcon from '~icons/solar/notification-unread-lines-bold-duotone';
+	import HomeIcon from '~icons/solar/home-bold-duotone';
+	import HistoryIcon from '~icons/solar/sort-by-time-bold-duotone';
+	import ConfigureIcon from '~icons/solar/settings-bold-duotone';
+	import { page } from '$app/stores';
+</script>
+
+<div class="header">
 	<div class="logo-area">
 		<img class="logo" src="/oguard.svg" alt="OGuard Logo" />
 
 		<h1 class="title">OGuard</h1>
 	</div>
-	<div></div>
+</div>
+<div class="sub-header">
+	<nav class="nav">
+		<ul class="nav-list">
+			<li class="nav-list__item" class:nav-link--selected={$page.route.id == '/'}>
+				<a class="nav-link" href="{base}/">
+					<HomeIcon class="nav-lin__item" />
+					Home
+				</a>
+			</li>
+			<li class="nav-list__item">
+				<a
+					class="nav-link"
+					href="{base}/events"
+					class:nav-link--selected={$page.route.id == '/events'}
+				>
+					<EventsIcon class="nav-lin__item" />
+					Events
+				</a>
+			</li>
+			<li class="nav-list__item">
+				<a
+					class="nav-link"
+					href="{base}/history"
+					class:nav-link--selected={$page.route.id == '/history'}
+				>
+					<HistoryIcon class="nav-lin__item" />
+					History
+				</a>
+			</li>
+			<li class="nav-list__item">
+				<a
+					class="nav-link"
+					href="{base}/configure"
+					class:nav-link--selected={$page.route.id == '/configure'}
+				>
+					<ConfigureIcon class="nav-lin__item" />
+					Configure
+				</a>
+			</li>
+		</ul>
+	</nav>
 </div>
 
 <style lang="scss">
-	.sidebar {
+	.header {
 		min-width: 15rem;
 		border: 1px solid #efefef;
 		box-shadow: 2px 10px 20px rgba(0, 0, 0, 0.1);
@@ -17,13 +68,52 @@
 		display: flex;
 		align-items: center;
 
-		justify-content: space-between;
 		flex-flow: row;
+		gap: 1rem;
+	}
+	.sub-header {
+		min-width: 15rem;
+		background-color: #34495e;
+		box-shadow: 2px 10px 20px rgba(0, 0, 0, 0.5);
+		text-align: center;
+		display: flex;
+		align-items: center;
+
+		flex-flow: row;
+		gap: 1rem;
+		margin-bottom: 0.5rem;
+	}
+
+	.nav {
+	}
+
+	.nav-list {
+		display: flex;
+		flex-flow: row;
+		list-style: none;
+		gap: 1.5rem;
+		align-items: center;
+
+		&__item {
+		}
+	}
+
+	.nav-link {
+		display: flex;
+		gap: 0.25rem;
+		text-decoration: none;
+		color: #fff;
+		padding: 1rem;
+
+		&--selected {
+			border-bottom: 0.2rem solid #ffffff;
+		}
 	}
 
 	.title {
 		text-transform: uppercase;
 		color: #34495e;
+		font-size: 1.5rem;
 	}
 
 	.logo-area {
@@ -33,6 +123,6 @@
 	}
 
 	.logo {
-		height: 3em;
+		height: 2em;
 	}
 </style>
