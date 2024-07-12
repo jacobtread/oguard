54231afe11a5dcce503b306eb11e02871b6f13ad
Jacobtread <jacobtread@gmail.com>
2024-07-12 18:47:42 +1200

    feat: executable config options

--- webapp/src/lib/components/pipeline/config/ExecutableConfig.svelte
+++ webapp/src/lib/components/pipeline/config/ExecutableConfig.svelte
@@ -1,9 +1,58 @@
 <script lang="ts">
 	import type { ActionTypeConfig, ActionTypeKey } from '$lib/api/types';
+	import DurationInput from '$lib/components/DurationInput.svelte';
 
 	export let config: ActionTypeConfig<ActionTypeKey.Executable>;
+
+	const addTimeout = () => (config.timeout = { secs: 100, nanos: 0 });
+	const removeTimeout = () => (config.timeout = null);
+
+	let arg = config.args.join(' ');
+
+	function updateArgs() {
+		config.args = arg.split(' ');
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+	$: arg, updateArgs();
 </script>
 
 <p>Executable config</p>
 
-<input type="text" bind:value={config.exe} />
+<div>
+	<h4>Executable</h4>
+
+	<p>
+		The executable to run, can be just the executable name if the executable is on the PATH
+		otherwise use an absolute path
+	</p>
+
+	<input type="text" bind:value={config.exe} required />
+</div>
+
+<div>
+	<h4>Arguments</h4>
+
+	<p>Arguments to run the executable with</p>
+
+	<input type="text" bind:value={arg} required />
+</div>
+
+<p>Will run</p>
+<pre><code>{config.exe} {config.args.join(' ')}</code></pre>
+
+<div>
+	<h4>Timeout</h4>
+
+	<p>
+		If the execution takes longer than a fixed time stop the program and consider it a failed
+		execution
+	</p>
+
+	{#if config.timeout === null}
+		<button on:click={addTimeout}>Add Timeout</button>
+	{:else}
+		<DurationInput bind:duration={config.timeout} />
+		<button on:click={removeTimeout}>Remove Timeout</button>
+	{/if}
+</div>
