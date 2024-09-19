1e99d4b804e82c9a64a9fa81b3ec237741b660f8
Jacobtread <jacobtread@gmail.com>
2024-09-19 22:07:33 +1200

    feat: hamburger menu nav for smaller screens

--- webapp/src/lib/components/Header.svelte
+++ webapp/src/lib/components/Header.svelte
@@ -6,6 +6,7 @@
 	import HistoryIcon from '~icons/solar/sort-by-time-bold-duotone';
 	import ConfigureIcon from '~icons/solar/settings-bold-duotone';
 	import PipelineIcon from '~icons/solar/square-sort-horizontal-bold-duotone';
+	import MenuIcon from '~icons/solar/hamburger-menu-outline';
 
 	import { page } from '$app/stores';
 	import { t } from 'svelte-i18n';
@@ -13,6 +14,8 @@
 	import { createLoginStateQuery } from '$lib/api/login';
 
 	const loginStateQuery = createLoginStateQuery();
+
+	let navVisible = false;
 </script>
 
 <div class="header">
@@ -21,68 +24,79 @@
 
 		<h1 class="title">OGuard</h1>
 	</div>
-
-	<nav class="nav">
-		<ul class="nav-list">
-			<li class="nav-list__item">
-				<a class="nav-link" href="{base}/" class:nav-link--selected={$page.route.id == '/'}>
-					<HomeIcon class="nav-link__item" />
-					{$t('pages.home')}
-				</a>
-			</li>
-			<li class="nav-list__item">
-				<a
-					class="nav-link"
-					href="{base}/events"
-					class:nav-link--selected={$page.route.id == '/events'}>
-					<EventsIcon class="nav-link__item" />
-					{$t('pages.events')}
-				</a>
-			</li>
-			<li class="nav-list__item">
-				<a
-					class="nav-link"
-					href="{base}/history"
-					class:nav-link--selected={$page.route.id == '/history'}>
-					<HistoryIcon class="nav-link__item" />
-					{$t('pages.history')}
-				</a>
-			</li>
-			<li class="nav-list__item">
-				<a
-					class="nav-link"
-					href="{base}/pipelines"
-					class:nav-link--selected={$page.route.id?.startsWith('/pipelines')}>
-					<PipelineIcon class="nav-link__item" />
-					{$t('pages.pipelines')}
-				</a>
-			</li>
-			<li class="nav-list__item">
-				<a
-					class="nav-link"
-					href="{base}/configure"
-					class:nav-link--selected={$page.route.id == '/configure'}>
-					<ConfigureIcon class="nav-link__item" />
-					{$t('pages.configure')}
-				</a>
-			</li>
-			<li class="nav-list__item">
-				<a
-					class="nav-link"
-					href="{base}/realtime"
-					class:nav-link--selected={$page.route.id == '/realtime'}>
-					<ConfigureIcon class="nav-link__item" />
-					{$t('pages.realtime')}
-				</a>
-			</li>
-		</ul>
-	</nav>
-
-	{#if $loginStateQuery.data !== undefined && $loginStateQuery.data.logged_in}
-		<div class="end-actions">
-			<LogoutButton />
+	<div class="nav-wrapper">
+		<div
+			class="nav-bg"
+			class:nav-bg--visible={navVisible}
+			aria-hidden="true"
+			on:click={() => (navVisible = false)}>
+			<nav class="nav">
+				<ul class="nav-list">
+					<li class="nav-list__item">
+						<a class="nav-link" href="{base}/" class:nav-link--selected={$page.route.id == '/'}>
+							<HomeIcon class="nav-link__item" />
+							{$t('pages.home')}
+						</a>
+					</li>
+					<li class="nav-list__item">
+						<a
+							class="nav-link"
+							href="{base}/events"
+							class:nav-link--selected={$page.route.id == '/events'}>
+							<EventsIcon class="nav-link__item" />
+							{$t('pages.events')}
+						</a>
+					</li>
+					<li class="nav-list__item">
+						<a
+							class="nav-link"
+							href="{base}/history"
+							class:nav-link--selected={$page.route.id == '/history'}>
+							<HistoryIcon class="nav-link__item" />
+							{$t('pages.history')}
+						</a>
+					</li>
+					<li class="nav-list__item">
+						<a
+							class="nav-link"
+							href="{base}/pipelines"
+							class:nav-link--selected={$page.route.id?.startsWith('/pipelines')}>
+							<PipelineIcon class="nav-link__item" />
+							{$t('pages.pipelines')}
+						</a>
+					</li>
+					<li class="nav-list__item">
+						<a
+							class="nav-link"
+							href="{base}/configure"
+							class:nav-link--selected={$page.route.id == '/configure'}>
+							<ConfigureIcon class="nav-link__item" />
+							{$t('pages.configure')}
+						</a>
+					</li>
+					<li class="nav-list__item">
+						<a
+							class="nav-link"
+							href="{base}/realtime"
+							class:nav-link--selected={$page.route.id == '/realtime'}>
+							<ConfigureIcon class="nav-link__item" />
+							{$t('pages.realtime')}
+						</a>
+					</li>
+				</ul>
+			</nav>
 		</div>
-	{/if}
+	</div>
+
+	<div class="end-actions">
+		<button class="button menu-button" on:click={() => (navVisible = !navVisible)}>
+			<MenuIcon />
+		</button>
+
+		{#if $loginStateQuery.data !== undefined && $loginStateQuery.data.logged_in}
+			<LogoutButton />
+		{/if}
+	</div>
 </div>
 
 <style lang="scss">
@@ -99,13 +113,17 @@
 		flex-flow: row;
 		gap: 1rem;
 		padding-left: 1rem;
+		position: relative;
+
+		min-height: 3.25rem;
+		height: 3.25rem;
 	}
 
 	.end-actions {
 		padding-right: 0.5rem;
 	}
 
-	.nav {
+	.nav-wrapper {
 		flex: auto;
 	}
 
@@ -114,6 +132,7 @@
 		flex-flow: row;
 		list-style: none;
 		align-items: center;
+		flex-wrap: wrap;
 	}
 
 	.nav-link {
@@ -128,6 +147,50 @@
 		&--selected {
 			background: palette.$gray-700;
 			color: #fff !important;
+		}
+	}
+
+	.menu-button {
+		padding: 0.1rem 0.25rem;
+		font-size: 1.3rem;
+		display: none;
+		line-height: 1;
+	}
+
+	@media screen and (max-width: 70rem) {
+		.menu-button {
+			display: block;
+		}
+
+		.nav {
+			background-color: palette.$gray-200;
+		}
+
+		.nav-bg {
+			display: none;
+			position: absolute;
+
+			left: 0;
+			top: 100%;
+
+			width: 100%;
+			height: calc(100vh - 3.25rem);
+
+			background-color: rgba($color: #000000, $alpha: 0.5);
+			z-index: 1;
+		}
+
+		.nav-bg--visible {
+			display: block;
+		}
+
+		.nav-list {
+			flex-flow: column;
+			align-items: stretch;
+		}
+
+		.nav-link {
+			width: 100%;
 		}
 	}
 
