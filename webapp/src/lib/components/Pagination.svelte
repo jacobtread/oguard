965d8254b72767b63107f13096d1d2762e92acbb
Jacobtread <jacobtread@gmail.com>
2024-09-20 21:34:47 +1200

    fix: hide pagination when no pages are available

--- webapp/src/lib/components/Pagination.svelte
+++ webapp/src/lib/components/Pagination.svelte
@@ -16,28 +16,30 @@
 	}
 </script>
 
-<!-- Pagination buttons -->
-<Pagination.Root {count} page={pageIndex + 1} {onPageChange} {perPage} let:pages>
-	<!-- Pagination count selector -->
-	<select bind:value={perPage} data-pagination-per-page-button>
-		<option value={5}>5</option>
-		<option value={10} selected>10</option>
-		<option value={20}>20</option>
-		<option value={30}>30</option>
-		<option value={50}>50</option>
-	</select>
+{#if count > 0}
+	<!-- Pagination buttons -->
+	<Pagination.Root {count} page={pageIndex + 1} {onPageChange} {perPage} let:pages>
+		<!-- Pagination count selector -->
+		<select bind:value={perPage} data-pagination-per-page-button>
+			<option value={5}>5</option>
+			<option value={10} selected>10</option>
+			<option value={20}>20</option>
+			<option value={30}>30</option>
+			<option value={50}>50</option>
+		</select>
 
-	<Pagination.PrevButton>Back</Pagination.PrevButton>
-	<div class="pagination-pages">
-		{#each pages as page (page.key)}
-			{#if page.type === 'ellipsis'}
-				<div data-pagination-page>...</div>
-			{:else}
-				<Pagination.Page {page}>
-					{page.value}
-				</Pagination.Page>
-			{/if}
-		{/each}
-	</div>
-	<Pagination.NextButton>Next</Pagination.NextButton>
-</Pagination.Root>
+		<Pagination.PrevButton>Back</Pagination.PrevButton>
+		<div class="pagination-pages">
+			{#each pages as page (page.key)}
+				{#if page.type === 'ellipsis'}
+					<div data-pagination-page>...</div>
+				{:else}
+					<Pagination.Page {page}>
+						{page.value}
+					</Pagination.Page>
+				{/if}
+			{/each}
+		</div>
+		<Pagination.NextButton>Next</Pagination.NextButton>
+	</Pagination.Root>
+{/if}
