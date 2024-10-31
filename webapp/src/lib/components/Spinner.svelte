0c8d703efe5d516a95ab6e74422852ea7acd5f6e
Jacobtread <jacobtread@gmail.com>
2024-10-31 22:35:55 +1300

    chore: format spinner

--- webapp/src/lib/components/Spinner.svelte
+++ webapp/src/lib/components/Spinner.svelte
@@ -10,6 +10,7 @@
 	.lds-ring div {
 		box-sizing: border-box;
 	}
+
 	.lds-ring {
 		display: inline-block;
 		position: relative;
@@ -17,6 +18,7 @@
 		height: 1em;
 		vertical-align: middle;
 	}
+
 	.lds-ring div {
 		box-sizing: border-box;
 		display: block;
@@ -29,15 +31,19 @@
 		animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
 		border-color: currentColor transparent transparent transparent;
 	}
+
 	.lds-ring div:nth-child(1) {
 		animation-delay: -0.45s;
 	}
+
 	.lds-ring div:nth-child(2) {
 		animation-delay: -0.3s;
 	}
+
 	.lds-ring div:nth-child(3) {
 		animation-delay: -0.15s;
 	}
+
 	@keyframes lds-ring {
 		0% {
 			transform: rotate(0deg);
